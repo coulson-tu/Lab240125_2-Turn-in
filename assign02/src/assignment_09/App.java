5dafbfc5b20553a617de6ee1e0cea763244c317d
coulson-tu <156402192+coulson-tu@users.noreply.github.com>
2024-01-24 22:28:10 -0800

    modified App.java

--- assign02/src/assignment_09/App.java
+++ assign02/src/assignment_09/App.java
@@ -4,40 +4,39 @@ import ignore.TestingUtils;
 
 public class App {
 
-
 	/**
-
-	Given 2 positive int arguments (a, b), return whichever argument is 
-	nearest to the number 21 without going over.
-	Return 0 if they both go over 21. 
-	<br>
-	<br>
-
+	 * 
+	 * Given 2 positive int arguments (a, b), return whichever argument is
+	 * nearest to the number 21 without going over.
+	 * Return 0 if they both go over 21.
+	 * <br>
+	 * <br>
+	 * 
 	 * <b>EXPECTATIONS:</b><br>
-		nearestTwentyOne(19, 21)   <b>---></b> 21 <br>
-		nearestTwentyOne(21, 19)    <b>---></b> 21 <br>
-		nearestTwentyOne(19, 22) <b>---></b> 19 <br>
-		nearestTwentyOne(32, 22) <b>---></b> 0 <br>
+	 * nearestTwentyOne(19, 21) <b>---></b> 21 <br>
+	 * nearestTwentyOne(21, 19) <b>---></b> 21 <br>
+	 * nearestTwentyOne(19, 22) <b>---></b> 19 <br>
+	 * nearestTwentyOne(32, 22) <b>---></b> 0 <br>
 	 */
-	
+
 	public static int nearestTwentyOne(int a, int b) {
-		
-		return 0;
+		if (a > 21 && b > 21) {
+			return 0;
+		}
+		int diffA = Math.abs(21 - a);
+		int diffB = Math.abs(21 - b);
+		if ((diffA <= diffB || B > 21) && A < 21) {
+			return A;
+		} else if (B <= 21) {
+			return B;
+		}
+
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	//----------------------STARTING POINT OF PROGRAM. IGNORE BELOW --------------------//
-	public static void main(String args[]){
+	// ----------------------STARTING POINT OF PROGRAM. IGNORE BELOW
+	// --------------------//
+	public static void main(String args[]) {
 		TestingUtils.runTests();
-		
+
 	}
 }
